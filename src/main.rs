4aa0e9b0c70cfe6a01c3dde132331a503b95870a
quyen1903 <quyenng2k@gmail.com>
2023-12-13 00:43:12 +0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use crate::cli::Cli;
+use crate::errors::Result;
+
+mod block;
+mod blockchain;
+mod cli;
+mod errors;
+mod server;
+mod transaction;
+mod tx;
+mod utxoset;
+mod wallets;
+
+
+
+fn main()->Result<()> {
+    let mut cli = Cli::new()?;
+    cli.run()?;
+
+    Ok(())
+}
